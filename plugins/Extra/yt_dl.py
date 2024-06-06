6ec029f8c3f21fb42f1f93166801ad0c7b188055
Ravaan1 <166496631+Ravaan1@users.noreply.github.com>
2024-06-06 22:36:44 +0530

    Update yt_dl.py

--- plugins/Extra/yt_dl.py
+++ plugins/Extra/yt_dl.py
@@ -33,13 +33,13 @@ async def song(client, message):
         thumb_name = f'thumb{title}.jpg'
         thumb = requests.get(thumbnail, allow_redirects=True)
         open(thumb_name, 'wb').write(thumb.content)
-        performer = f"[VJ NETWORKS™]" 
+        performer = f"[ʜᴋ ᴘʀᴏᴅᴜᴄᴛɪᴏɴs™]" 
         duration = results[0]["duration"]
         url_suffix = results[0]["url_suffix"]
         views = results[0]["views"]
     except Exception as e:
         print(str(e))
-        return await m.edit("Example: /song vaa vaathi song")
+        return await m.edit("Example: /song Buttons")
                 
     await m.edit("**dσwnlσαdíng чσur ѕσng...!**")
     try:
@@ -48,7 +48,7 @@ async def song(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        cap = "**BY›› [VJ NETWORKS™](https://t.me/vj_bots)**"
+        cap = "**BY›› [ʜᴋ ᴘʀᴏᴅᴜᴄᴛɪᴏɴs™](https://t.me/TG_UPDATES1)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
